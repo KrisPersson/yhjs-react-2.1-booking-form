0996e67bbb3dcae3f1dfab9bd885576f5d99742d
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2023-01-27 11:11:12 +0100

    Update Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,32 +1,104 @@
 
 import "./Form.css"
 
+
+let formData = {
+    klass: '',
+    numTickets: '1',
+    title: '',
+    firstName: '',
+    lastName: '',
+    agreeTerms: false
+}
+
+function handleKlass(event) {
+    const klass = event.target.value
+
+    formData = {
+        ...formData,
+        klass: klass
+    }
+}
+
+function handleNumTickets(event) {
+    const numTickets = event.target.value
+
+    formData = {
+        ...formData,
+        numTickets: numTickets
+    }
+}
+
+function handleTitle(event) {
+    const title = event.target.value
+
+    formData = {
+        ...formData,
+        title: title
+    }
+}
+
+function handleFirstName(event) {
+    const firstName = event.target.value
+
+    formData = {
+        ...formData,
+        firstName: firstName
+    }
+}
+
+function handleLastName(event) {
+    const lastName = event.target.value
+
+    formData = {
+        ...formData,
+        lastName: lastName
+    }
+}
+
+function handleAgreeTerms(event) {
+    const agreeTerms = event.target.checked
+
+    formData = {
+        ...formData,
+        agreeTerms: agreeTerms
+    }
+
+}
+
+function handleBtnClick(event) {
+    console.log(formData)
+    event.preventDefault()
+}
+
+
+
 function Form() {
 
     return (
         <form action="#">
-            <section class='form__klass'>
-                <label class='label-radio' htmlFor="">
-                    <input class='form__radio' type="radio" name="klass" id="klass-1" />
+            <section className='form__klass'>
+                <label className='label-radio' htmlFor="">
+                    <input onClick={ handleKlass } className='form__radio' value="1" type="radio" name="klass" id="klass-1" />
                     <p>1:a klass</p>
                 </label>
-                <label class='label-radio' htmlFor="">
-                    <input class='form__radio' type="radio" name="klass" id="klass-2" />
+                <label className='label-radio' htmlFor="">
+                    <input onClick={ handleKlass }  className='form__radio' value="2" type="radio" name="klass" id="klass-2" />
                     <p>2:a klass</p>
                 </label>       
             </section>
-            <label class='label-short' htmlFor="">
+            <label className='label-short' htmlFor="">
                 Antal biljetter
-                <select name="amt-tickets" id="amt-tickets">
+                <select onChange={ handleNumTickets } name="amt-tickets" id="amt-tickets">
                     <option value="1">1</option>
-                    <option value="1">2</option>
-                    <option value="1">3</option>
-                    <option value="1">4+</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4+">4+</option>
                 </select>
             </label>
-            <label class='label-short' htmlFor="">
+            <label className='label-short' htmlFor="">
                 Titel
-                <select name="titel" id="titel">
+                <select onChange={ handleTitle } name="titel" id="titel">
                     <option value="-">-</option>
                     <option value="Ms">Ms</option>
                     <option value="Mrs">Mrs</option>
@@ -35,17 +107,17 @@ function Form() {
             </label>
             <label htmlFor="">
                 Förnamn
-                <input id='first-name' type="text" placeholder='Ditt förnamn' required />
+                <input onChange={ handleFirstName } id='first-name' type="text" placeholder='Ditt förnamn' required />
             </label>
             <label htmlFor="">
                 Efternamn
-                <input id='last-name' type="text" placeholder='Ditt efternamn' required />
+                <input onChange={ handleLastName }  id='last-name' type="text" placeholder='Ditt efternamn' required />
             </label>
-            <label class='accept-terms' htmlFor="">
+            <label onChange={ handleAgreeTerms }  className='accept-terms' htmlFor="">
                 <input type="checkbox" name="accept" id="accept" />
                 Godkänner villkoren
             </label>
-            <button>Boka biljetter</button>
+            <button onClick={ handleBtnClick }>Boka biljetter</button>
         </form>
     )
 }
